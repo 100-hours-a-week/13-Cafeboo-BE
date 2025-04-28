2ba8828a0bb041b3afe014cfcd35771ce1517420
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-04-28 15:31:01 +0900

    #0 스프링 부트 기본 설정 파일 추가
    추가한 dependency 목록
    1. Spring Web
    2. Spring Security
    3. Spring JPA
    4. PostgreSQL Driver
    5. Lombok
    6. Websocket
    7. OAuth2 Client
    8. Spring Redis
    9. Spring Boot Devtools

---
+++ cafeboo/src/test/java/com/ktb/cafeboo/CafebooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.cafeboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafebooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

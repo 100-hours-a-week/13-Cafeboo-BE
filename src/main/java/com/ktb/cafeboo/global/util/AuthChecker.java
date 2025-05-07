bf77993c0cf585a11ea5e00c19351c8240ee17be
Yunju07 <juile0827@naver.com>
2025-05-07 11:40:19 +0900

    ✨feat: 리소스 소유자 확인을 위한 AuthChecker 유틸 클래스 추가
     - ownerId와 currentUserId를 비교하여 권한 없는 접근 시 예외 발생
     - 리소스 기반 인가 로직을 공통 유틸로 분리하여 재사용성 향상

---
+++ src/main/java/com/ktb/cafeboo/global/util/AuthChecker.java
@@ -0,0 +1,13 @@
+package com.ktb.cafeboo.global.util;
+
+import com.ktb.cafeboo.global.apiPayload.code.status.ErrorStatus;
+import com.ktb.cafeboo.global.apiPayload.exception.CustomApiException;
+
+public class AuthChecker {
+
+    public static void checkOwnership(Long ownerId, Long currentUserId) {
+        if (!ownerId.equals(currentUserId)) {
+            throw new CustomApiException(ErrorStatus.FORBIDDEN);
+        }
+    }
+}

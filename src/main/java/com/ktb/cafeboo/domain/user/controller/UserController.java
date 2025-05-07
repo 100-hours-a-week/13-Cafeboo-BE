ac6d54f2431ed2d1b24ce15c0f5fee6250c5cea9
Yunju07 <juile0827@naver.com>
2025-05-07 09:30:40 +0900

    ✨feat: 유저 이메일 중복 조회 API 개발
     해당 이메일이 중복인지 확인하는 API 개발

--- src/main/java/com/ktb/cafeboo/domain/user/controller/UserController.java
+++ src/main/java/com/ktb/cafeboo/domain/user/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.ktb.cafeboo.domain.user.controller;
 
+import com.ktb.cafeboo.domain.user.dto.EmailDuplicationResponse;
+import com.ktb.cafeboo.domain.user.service.UserService;
+import com.ktb.cafeboo.global.apiPayload.ApiResponse;
+import com.ktb.cafeboo.global.apiPayload.code.status.SuccessStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping("/email")
+    public ResponseEntity<ApiResponse<EmailDuplicationResponse>> checkEmailDuplication(
+            @RequestParam String email) {
+        EmailDuplicationResponse response = userService.isEmailDuplicated(email);
+        return ResponseEntity.ok(ApiResponse.of(SuccessStatus.EMAIL_DUPLICATION_CHECK_SUCCESS, response));
+    }
 }

a7b9bd435ddb21f34435a23e292de74b428de770
Yunju07 <juile0827@naver.com>
2025-05-11 21:01:37 +0900

    ♻️refactor: 사용하지 않는 import문 제거

--- src/main/java/com/ktb/cafeboo/domain/recommend/service/CaffeineRecommendationService.java
+++ src/main/java/com/ktb/cafeboo/domain/recommend/service/CaffeineRecommendationService.java
@@ -10,8 +10,6 @@ import com.ktb.cafeboo.global.infra.ai.dto.PredictCaffeineLimitByRuleResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Slf4j

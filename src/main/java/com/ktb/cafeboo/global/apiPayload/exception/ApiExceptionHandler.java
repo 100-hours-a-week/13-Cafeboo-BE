97ea2ce95f8ec617eb6ac0a2357e8d469fa5b2f3
Yunju07 <juile0827@naver.com>
2025-05-12 10:54:37 +0900

    🐛feat: ApiExceptionHandler 내, 오류 로깅 추가

--- src/main/java/com/ktb/cafeboo/global/apiPayload/exception/ApiExceptionHandler.java
+++ src/main/java/com/ktb/cafeboo/global/apiPayload/exception/ApiExceptionHandler.java
@@ -3,16 +3,19 @@ package com.ktb.cafeboo.global.apiPayload.exception;
 import com.ktb.cafeboo.global.apiPayload.ApiResponse;
 import com.ktb.cafeboo.global.apiPayload.code.status.ErrorStatus;
 import com.ktb.cafeboo.global.apiPayload.code.BaseCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
     // 기본 서버 오류
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGenericException(Exception ex) {
+        log.error(ex.getMessage(), ex);
         return ResponseEntity
                 .status(ErrorStatus.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ApiResponse.onFailure(ErrorStatus.INTERNAL_SERVER_ERROR));
@@ -20,6 +23,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        log.error(ex.getMessage(), ex);
         return ResponseEntity
                 .badRequest()
                 .body(ApiResponse.onFailure(ErrorStatus.BAD_REQUEST));
@@ -27,6 +31,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(CustomApiException.class)
     public ResponseEntity<ApiResponse<Void>> handleCustomApiException(CustomApiException ex) {
+        log.error(ex.getMessage(), ex);
         BaseCode error = ex.getErrorCode();
         return ResponseEntity
                 .status(error.getStatus())

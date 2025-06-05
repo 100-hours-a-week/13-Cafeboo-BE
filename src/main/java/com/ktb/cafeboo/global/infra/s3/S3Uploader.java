e8cb3de8fcccb5e5139d5849c05c66c14b795b01
Yunju07 <juile0827@naver.com>
2025-06-05 16:48:47 +0900

    ✨ feat: S3 이미지 업로드 기능 개발
    - AWS S3에 이미지 파일을 업로드하기 위한 S3Uploader 클래스
    - 프로필 이미지와 리뷰 이미지는 업로드 실패 시 fallback 처리 방식이 달라, 업로더 클래스 내에 메서드를 분리하여 각각 대응하도록 설계

---
+++ src/main/java/com/ktb/cafeboo/global/infra/s3/S3Uploader.java
@@ -0,0 +1,88 @@
+package com.ktb.cafeboo.global.infra.s3;
+
+import com.ktb.cafeboo.global.apiPayload.code.status.ErrorStatus;
+import com.ktb.cafeboo.global.apiPayload.exception.CustomApiException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+    private final S3Properties s3Properties;
+
+    public String uploadProfileImage(InputStream inputStream, long contentLength, String contentType) {
+        String fileName = String.format("%s/profile-images/%s", s3Properties.getDir(), UUID.randomUUID());
+        try {
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(s3Properties.getBucket())
+                    .key(fileName)
+                    .contentType(contentType)
+                    .acl("public-read")
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromInputStream(inputStream, contentLength));
+
+            String uploadedUrl = generatePublicUrl(fileName);
+            log.info("[S3Uploader.uploadProfileImageOrDefault] 업로드 성공: {}", uploadedUrl);
+            return uploadedUrl;
+        } catch (Exception e) {
+            log.warn("[S3Uploader.uploadProfileImageOrDefault] 업로드 실패, 기본 이미지 사용: {}", e.getMessage());
+            return s3Properties.getDefaultProfileImageUrl();
+        }
+    }
+
+    public String uploadReviewImage(InputStream inputStream, long contentLength, String contentType) {
+        String fileName = String.format("%s/review-images/%s", s3Properties.getDir(), UUID.randomUUID());
+        try {
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(s3Properties.getBucket())
+                    .key(fileName)
+                    .contentType(contentType)
+                    .acl("public-read")
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromInputStream(inputStream, contentLength));
+
+            String uploadedUrl = generatePublicUrl(fileName);
+            log.info("[S3Uploader.uploadReviewImage] 업로드 성공: {}", uploadedUrl);
+            return uploadedUrl;
+        } catch (Exception e) {
+            log.error("[S3Uploader.uploadReviewImage] 업로드 실패", e);
+            throw new CustomApiException(ErrorStatus.S3_REVIEW_IMAGE_UPLOAD_FAILED);
+        }
+    }
+
+    public String generatePresignedUrl(String fileName, Duration duration) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(s3Properties.getBucket())
+                .key(fileName)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(duration)
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return s3Presigner.presignGetObject(presignRequest).url().toString();
+    }
+
+
+    private String generatePublicUrl(String key) {
+        return String.format("https://%s.s3.amazonaws.com/%s", s3Properties.getBucket(), key);
+    }
+}

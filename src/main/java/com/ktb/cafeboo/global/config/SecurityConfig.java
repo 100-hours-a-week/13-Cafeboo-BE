f483e46d1dcc98f1f45b480346ab39c271b473cc
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-06-09 10:29:00 +0900

    CHORE: CORS 오류 방지를 위한 옵션 추가
    채팅 기능 테스트 간 CORS 오류가 발생.
    
    해당 오류가 발생하지 않도록 제외 목록에 endpoint 추가

--- src/main/java/com/ktb/cafeboo/global/config/SecurityConfig.java
+++ src/main/java/com/ktb/cafeboo/global/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
             new AntPathRequestMatcher("/favicon.ico"),
             new AntPathRequestMatcher("/webjars/**"), // SockJS, STOMP.js가 webjars를 통해 제공된다면 필요// 웹소켓 엔드포인트
             new AntPathRequestMatcher("/api/chat/**"),
-            new AntPathRequestMatcher("/api/chatrooms/**")
+            new AntPathRequestMatcher("/api/chatrooms/**"),
+            new AntPathRequestMatcher("/chatrooms/{roomId}/member") // 테스트용
         );
 
         http

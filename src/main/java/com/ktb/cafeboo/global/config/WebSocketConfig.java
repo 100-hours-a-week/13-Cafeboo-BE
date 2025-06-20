8578ef9be00ade7e3badeda7e998544e276339fa
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-06-20 17:27:22 +0900

    [FIX] BE heartbeat 값 설정
    heartbeat 설정 추가

--- src/main/java/com/ktb/cafeboo/global/config/WebSocketConfig.java
+++ src/main/java/com/ktb/cafeboo/global/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 1. /topic으로 시작하는 메시지를 브로커가 처리하도록 설정 (Pub/Sub)
         // 2. /queue로 시작하는 메시지를 브로커가 처리하도록 설정 (개인 메시지)
         // Spring의 Simple Broker (인메모리) 사용. 실제 서비스에서는 Redis, Kafka, RabbitMQ 등의 외부 브로커 사용 권장.
-        config.enableSimpleBroker("/topic", "/queue");
+        config.enableSimpleBroker("/topic", "/queue").setHeartbeatValue(new long[] {10000, 10000});
 
         // 클라이언트가 서버로 메시지를 보낼 때 사용할 접두사 (Controller로 라우팅됨)
         config.setApplicationDestinationPrefixes("/app");
@@ -30,6 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 클라이언트가 http://localhost:8080/ws 로 연결 시도
         registry.addEndpoint("/ws")
             .setAllowedOriginPatterns("*") // 모든 Origin 허용 (CORS). 실제 운영에서는 특정 Origin만 허용해야 함
-            .withSockJS(); // WebSocket을 지원하지 않는 브라우저를 위해 SockJS fallback 활성화
+            .withSockJS()
+            .setDisconnectDelay(300 * 1000);
     }
 }

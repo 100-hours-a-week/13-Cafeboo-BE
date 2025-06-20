46859d42120e2381548b623f3c9d235646b77bd2
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-06-20 17:28:21 +0900

    [FIX] heartbeat 값 수정
    heartbeat 값 수정

--- src/main/java/com/ktb/cafeboo/global/config/WebSocketConfig.java
+++ src/main/java/com/ktb/cafeboo/global/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 1. /topic으로 시작하는 메시지를 브로커가 처리하도록 설정 (Pub/Sub)
         // 2. /queue로 시작하는 메시지를 브로커가 처리하도록 설정 (개인 메시지)
         // Spring의 Simple Broker (인메모리) 사용. 실제 서비스에서는 Redis, Kafka, RabbitMQ 등의 외부 브로커 사용 권장.
-        config.enableSimpleBroker("/topic", "/queue").setHeartbeatValue(new long[] {10000, 10000});
+        config.enableSimpleBroker("/topic", "/queue").setHeartbeatValue(new long[] {10000, 0});
 
         // 클라이언트가 서버로 메시지를 보낼 때 사용할 접두사 (Controller로 라우팅됨)
         config.setApplicationDestinationPrefixes("/app");

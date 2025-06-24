722680480cef9930f112aa238426f0ecf210c9fe
Yunju07 <juile0827@naver.com>
2025-06-24 20:34:13 +0900

    🐛fix: 단위 테스트 환경에서 JwtAuthenticationFilter 비활성화를 위한 @Profile("!test") 추가
    - 단위 테스트 수행 시 보안 필터 적용 방지

--- src/main/java/com/ktb/cafeboo/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ktb/cafeboo/global/security/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {

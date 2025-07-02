3f5802d07f94118156ef946cec1d27fb8bb8caa4
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-07-02 16:51:16 +0900

    [FEAT] 음료 추천 모델 추가에 따른 DTO 추가
    AI를 활용한 음료 추천 기능이 추가되었고 이에 따라 BE에서 AI로 음료 추천 목록 생성 요청 및 응답을 받을 때 필요한 DTO의 추가를 진행했습니다.

---
+++ src/main/java/com/ktb/cafeboo/global/infra/ai/dto/CreateDrinkRecommendationResponse.java
@@ -0,0 +1,46 @@
+package com.ktb.cafeboo.global.infra.ai.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CreateDrinkRecommendationResponse {
+    private String status;
+    private String message;
+    private Data data;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class Data {
+        private String status;
+        private RecommendationDetails data;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class RecommendationDetails {
+        private List<Recommendation> recommendedDrinks;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class Recommendation {
+        private Long drinkId;
+        private Double score;
+    }
+}

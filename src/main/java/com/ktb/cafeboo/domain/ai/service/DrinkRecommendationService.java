d4bb899ec5bbfd02868440cd0714a1cd86c3e6e4
LeRelais <81596262+LeRelais@users.noreply.github.com>
2025-07-09 15:46:11 +0900

    [REFACTOR] 커피 추천 요청 시 빠진 파라미터 추가
    AI 서버에 커피 추천 요청을 보낼 시 누락되었던 파라미터 정보를 추가해서 보내도록 수정 진행했습니다.

--- src/main/java/com/ktb/cafeboo/domain/ai/service/DrinkRecommendationService.java
+++ src/main/java/com/ktb/cafeboo/domain/ai/service/DrinkRecommendationService.java
@@ -1,5 +1,9 @@
 package com.ktb.cafeboo.domain.ai.service;
 
+import com.ktb.cafeboo.domain.report.dto.WeeklyCaffeineReportResponse;
+import com.ktb.cafeboo.domain.report.model.DailyStatistics;
+import com.ktb.cafeboo.domain.report.service.DailyStatisticsService;
+import com.ktb.cafeboo.domain.report.service.WeeklyReportService;
 import com.ktb.cafeboo.domain.user.model.User;
 import com.ktb.cafeboo.domain.user.model.UserCaffeineInfo;
 import com.ktb.cafeboo.domain.user.model.UserHealthInfo;
@@ -9,6 +13,10 @@ import com.ktb.cafeboo.global.apiPayload.exception.CustomApiException;
 import com.ktb.cafeboo.global.infra.ai.client.AiServerClient;
 import com.ktb.cafeboo.global.infra.ai.dto.CreateDrinkRecommendationRequest;
 import com.ktb.cafeboo.global.infra.ai.dto.CreateDrinkRecommendationResponse;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.List;
+import java.util.Locale;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,6 +27,8 @@ import org.springframework.stereotype.Service;
 public class DrinkRecommendationService {
     private final AiServerClient aiServerClient;
     private final UserService userService;
+    private final DailyStatisticsService dailyStatisticsService;
+    private final WeeklyReportService weeklyReportService;
 
     public CreateDrinkRecommendationResponse getRecommendationResult(Long userId){
         log.info("[DrinkRecommendationService.getRecommendationResult] - userId : {}", userId);
@@ -36,19 +46,34 @@ public class DrinkRecommendationService {
             throw new CustomApiException(ErrorStatus.CAFFEINE_PROFILE_NOT_FOUND);
         }
 
+        LocalDate now = LocalDate.now();
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+
+        String targetYear = String.valueOf(now.getYear());
+        String targetMonth = String.valueOf(now.getMonthValue());
+        String targetWeek = String.valueOf(now.get(weekFields.weekOfWeekBasedYear()));
+
+        List<DailyStatistics> dailyStats = dailyStatisticsService.getDailyStatisticsForWeek(userId, targetYear, targetMonth, targetWeek);
+
+        WeeklyCaffeineReportResponse weeklyCaffeineReportResponse = weeklyReportService.getWeeklyReport(userId, targetYear, targetMonth, targetWeek, dailyStats);
+
         CreateDrinkRecommendationRequest request = CreateDrinkRecommendationRequest.builder()
             .gender(healthInfo.getGender())
             .age(healthInfo.getAge())
-            .weight(healthInfo.getWeight())
             .height(healthInfo.getHeight())
+            .weight(healthInfo.getWeight())
             .isSmoking(healthInfo.getSmoking() ? 1 : 0)
             .isTakingBirthPill(healthInfo.getTakingBirthPill() ? 1 : 0)
             .isPregnant(healthInfo.getPregnant() ? 1 : 0)
             .caffeineSensitivity(caffeineInfo.getCaffeineSensitivity())
+            .avgIntakePerDay(caffeineInfo.getAverageDailyCaffeineIntake())
+            .avgCaffeineAmount(weeklyCaffeineReportResponse.dailyCaffeineAvg())
+            .dailyCaffeineLimit(caffeineInfo.getDailyCaffeineLimitMg())
             .build();
 
         log.info("[DrinkRecommendationService.getRecommendationResult] - AI 추천 결과 생성 요청");
         CreateDrinkRecommendationResponse response = aiServerClient.createCoffeeRecommendation(request);
+        log.info("AI 서버 응답 DTO: {}", response);
         log.info("[DrinkRecommendationService.getRecommendationResult] - AI 추천 결과 생성 성공");
 
         if (!"success".equals(response.getStatus())) {
